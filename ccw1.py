e2ab2454f7f21509308bb9ec3a6793f30056c87a
Neeraj Kumar <neerajkumar4596@gmail.com>
2021-06-09 19:34:40 +0530

    First Commit

---
+++ ccw1.py
@@ -0,0 +1,15 @@
+###Make a User Profile
+
+
+print("Hii,Please Fill Your Basic Info: ")
+
+
+UserName = input("Enter Your Name:\n")
+UserMobileNumber = int(input("Enter Your Mobile Number:\n"))
+UserAge = int(input("Enter Your Age:\n"))
+UserAddress = input("Enter Your Address:\n")
+
+print(type(UserName))
+print(type(UserMobileNumber))
+print(type(UserAge))
+print(type(UserAddress))
